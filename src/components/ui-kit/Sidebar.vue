f7f6883986576fe1f88d05434cf0f9a1f36218b3
235 <krahs@gmai.com>
2024-07-12 01:27:31 +0500

    edit style sidebar

--- src/components/ui-kit/Sidebar.vue
+++ src/components/ui-kit/Sidebar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="d-flex flex-column flex-shrink-0 p-3 bg-light sidebar">
+  <div class="d-flex flex-column flex-shrink-0 p-3 sidebar">
     <ul class="nav nav-pills flex-column mb-auto">
       <li class="nav-item">
         <router-link to="/users" class="nav-link d-flex align-items-center" active-class="active">
@@ -37,10 +37,21 @@
           Coinflip
         </router-link>
       </li>
+      <li>
+        <router-link to="/wheeltest/3" class="nav-link d-flex align-items-center" active-class="active">
+          <span class="material-icons me-2">games</span>
+          Test Wheel
+        </router-link>
+      </li>
+      <li>
+        <router-link to="/coinfliptest/3" class="nav-link d-flex align-items-center" active-class="active">
+          <span class="material-icons me-2">monetization_on</span>
+          Test Coinflip
+        </router-link>
+      </li>
     </ul>
-    <hr>
-    <div class="dropdown">
-      <a href="#" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+    <div class="admin-card dropdown">
+      <a href="#" class="d-flex align-items-center text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
         <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
         <strong>Админ</strong>
       </a>
@@ -60,24 +71,52 @@ import { useAuth } from "@/composables/admin/useLogout.js";
 const { logout } = useAuth();
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .sidebar {
   height: 100vh;
   position: fixed;
   overflow-y: auto;
-  width: 15%;
+  width: 15rem;
+  left: 0;
+  top: 0;
+  background: var(--Surface-Surface-2, #1a1c25);
+}
+
+.nav {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
 }
 
 .nav-link {
   display: flex;
   align-items: center;
+  color: #a8abba;
+  transition: all 0.3s ease-in-out;
+
+  &:hover {
+    background-color: #21232F;
+  }
 }
 
 .nav-link .material-icons {
   font-size: 24px;
 }
 
-.nav-link.active {
-  background-color: #e9ecef;
+.admin-card {
+  a {
+    color: #fff;
+  }
+
+  .dropdown-menu {
+    background-color: #21232f;
+
+    .dropdown-item {
+      transition: all 0.3s ease-in-out;
+      &:hover {
+        background: var(--surface-surface-4, #2a2c3b);
+      }
+    }
+  }
 }
 </style>
